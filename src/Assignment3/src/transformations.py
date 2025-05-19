c2ba8b71d8aa6386478b8ab081de5c31fd74cf56
AmirhosseinSoltan <ah.soltani2010@gmail.com>
2025-05-19 18:12:54 +0200

    Transformations updated

--- src/Assignment3/src/transformations.py
+++ src/Assignment3/src/transformations.py
@@ -8,14 +8,16 @@ class RandomTemporalSampling: #---> Good to use but does not preserve the tensor
     Example:
         Given a sequence of 40 frames, it will return 20 frames.
     """
-    def __init__(self, p=0.5):
-        # Parameter p is kept for compatibility, but not used anymore
-        pass
+    def __init__(self, slicing_step):
+        # We slice the frames with a step size of slicing_step to include more semantically important sequence frames
+        # This is a good trade-off between the number of frames and the semantic importance of the frames
         
+        self.slicing_step = slicing_step
+
     def __call__(self, frames):
         # Always use step size 2 if there are enough frames
         if frames.size(0) > 2:
-            return frames[::4]
+            return frames[::self.slicing_step]
         return frames
 
 class RandomTemporalReverse:
@@ -35,7 +37,7 @@ class RandomTemporalReverse:
 
 
 # Combined transforms
-def get_train_transforms():
+def get_train_transforms(slicing_step):
     """
     Returns the composition of transforms for training.
     """
@@ -44,14 +46,14 @@ def get_train_transforms():
         transforms.RandomHorizontalFlip(p=0.5),
         transforms.RandomRotation(25),
         # Temporal augmentations
-        RandomTemporalSampling(p=1),
+        RandomTemporalSampling(slicing_step),
         RandomTemporalReverse(p=0.3),
     ])
 
-def get_test_transforms():
+def get_test_transforms(slicing_step):
     """
     Returns the composition of transforms for testing.
     """
     return transforms.Compose([
-        RandomTemporalSampling(p=1)
+        RandomTemporalSampling(slicing_step)
     ])
