fde1a82317a797d21088bfa4502bd119e5cb94ff
AmirhosseinSoltan <ah.soltani2010@gmail.com>
2025-06-16 21:13:09 +0200

    added DCGAN

---
+++ src/Assignment5/DCGAN.py
@@ -0,0 +1,99 @@
+#!/usr/bin/env python
+
+import os
+import shutil
+from tqdm import tqdm
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+from pytorch_lightning import seed_everything
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torchvision
+from torchvision import datasets, models, transforms
+from torchvision.utils import save_image
+from torch.utils.data import DataLoader
+from utils import *
+import models
+# from models import Generator, Discriminator, Trainer ---> imports statically, not compatibel with %load_ext autoreload
+from torch.utils.tensorboard import SummaryWriter
+
+
+configs = {   
+    "model_name" : "DCGAN",
+    "exp" : "1",  
+    "latent_dim" : 128,
+    "batch_size" : 64,
+    "num_epochs" : 50,
+    "lr" : 1e-3,
+    "scheduler" : "ReduceLROnPlateau",
+    "use_scheduler" : True,
+    }
+
+dataset_root = '../Assignment4/data/AFHQ/'
+
+transform = transforms.Compose([transforms.Resize((64,64)),
+                                      transforms.ToTensor(),
+                                      transforms.Normalize([0.5]*3 , [0.5]*3)])
+
+BS = configs["batch_size"]
+latent_dim = configs["latent_dim"]
+
+train_dataset = datasets.ImageFolder(root= dataset_root+'train', transform= transform )
+test_dataset = datasets.ImageFolder(root= dataset_root+'test', transform= transform )
+
+# print(train_dataset.classes)  
+print(train_dataset.class_to_idx)  
+
+train_loader = DataLoader(dataset= train_dataset, 
+                          batch_size= BS, 
+                          shuffle= True, 
+                          drop_last= True )
+
+test_loader = DataLoader(dataset= test_dataset, 
+                          batch_size= BS, 
+                          shuffle= False, 
+                          drop_last= True )
+
+
+generator = models.Generator(latent_dim=latent_dim, num_channels=3, base_channels=64)
+# print(generator)
+
+
+gen_img = generator(torch.rand(BS, 128, 1, 1))
+print(f'output shape: {gen_img.shape}')
+
+assert gen_img.shape == (BS, 3, 64, 64), "Generator output shape is incorrect! The Generator should output a fake image equal to the size of the training images"
+
+
+discriminator = models.Discriminator(in_channels=3, out_dim=1, base_channels=64)
+# print(discriminator)
+
+
+desc_input = torch.rand(BS, 3, 64, 64)
+desc_output = discriminator(desc_input)
+print(f'output shape: {desc_output.shape}')
+assert desc_output.shape == (BS, 1, 1, 1), "Discriminator output shape is incorrect! The Discriminator should output a single value"
+
+
+count_model_params(discriminator)
+
+
+count_model_params(generator)
+
+seed_everything(42)
+
+model_name = configs["model_name"]+configs["exp"]
+savepath, writer = makedires(configs)
+
+model = models.Trainer(generator=generator, discriminator=discriminator, latent_dim=latent_dim, writer=writer)
+
+epoch = configs["num_epochs"]
+
+model.train(data_loader=train_loader)
+
+save_model(model, model_name, model.optim_generator, model.optim_discriminator, epoch = epoch, stats = configs )
+save_config(configs)
+
