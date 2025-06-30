cfc23e6b35402ba6df1140c607935baec61dff71
AmirhosseinSoltan <ah.soltani2010@gmail.com>
2025-06-30 23:19:47 +0200

    models updated

--- src/Assignment6/models.py
+++ src/Assignment6/models.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torchvision import models
+import torch.nn.functional as F
 import utils
 
 
@@ -13,9 +14,17 @@ class SiameseModel(nn.Module):
         super().__init__()
 
         # initialize resnet as backbone for feature extraction
-        self.resnet = models.resnet18(weights=ResNet18_Weights.DEFAULT)
+        self.resnet = models.resnet18(pretrained=False)
         self.resnet.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
-        self.resnet.fc = nn.Linear(self.resnet.fc.in_features, emb_dim)
+        # self.resnet.fc = nn.Linear(self.resnet.fc.in_features, emb_dim)
+        self.resnet.fc = nn.Identity()
+
+        # projection head
+        self.proj_head = nn.Sequential(
+                nn.Linear(512, emb_dim),
+                nn.ReLU(),
+                nn.Linear(emb_dim, emb_dim)
+            )
         self.cnn = self.resnet
 
         # auxiliar layers
@@ -26,9 +35,7 @@ class SiameseModel(nn.Module):
     
     def forward_one(self, x):
         """ Forwarding just one sample through the model """
-        x_emb = self.cnn(x)
-        # x_flat = self.flatten(x)
-        # x_emb = self.fc(x)
+        x_emb = self.proj_head(self.cnn(x))
         x_emb_norm = self.norm(x_emb)
         return x_emb_norm
     
@@ -44,3 +51,31 @@ class SiameseModel(nn.Module):
         anchor_emb, positive_emb, negative_emb = all_embs.chunk(3, dim=0)
         
         return anchor_emb, positive_emb, negative_emb
+    
+
+
+class SimCLR(nn.Module):
+        """
+        SimCLR model with ResNet-18 Backbone
+        """
+        def __init__(self, hidden_dim=512, output_dim=128):
+            super().__init__()
+
+            # Use a pre-trained ResNet18 but modify for grayscale input
+            self.backbone = models.resnet18(weights=None)
+            self.backbone.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+            self.backbone.fc = nn.Identity()
+
+            # projection head
+            self.proj_head = nn.Sequential(
+                    nn.Linear(512, hidden_dim),
+                    nn.ReLU(),
+                    nn.Linear(hidden_dim, output_dim)
+                )
+            return
+        
+        def forward(self, x):
+            h = self.backbone(x)
+            z = self.proj_head(h)
+            out = F.normalize(z, dim=1)  # L2 normalize
+            return out
