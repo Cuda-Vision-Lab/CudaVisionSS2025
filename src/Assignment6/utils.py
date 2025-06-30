07858e8b25bac3cc1ba9ee52365480ed7497b2a4
AmirhosseinSoltan <ah.soltani2010@gmail.com>
2025-06-30 21:48:23 +0200

    utils updated

--- src/Assignment6/utils.py
+++ src/Assignment6/utils.py
@@ -9,6 +9,8 @@ from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from sklearn.metrics import adjusted_rand_score
 from matplotlib.offsetbox import OffsetImage, AnnotationBbox
+from torchvision import datasets, models, transforms
+import torch.nn.functional as F
 import random
 
 def smooth(f, K=5):
@@ -87,28 +89,6 @@ def visualize_progress(train_loss, val_loss, start=0):
 
     return
 
-def display_projections(points, labels, ax=None, legend=None):
-    """ Displaying low-dimensional data projections """
-    
-    COLORS = ['r', 'b', 'g', 'y', 'purple', 'orange', 'k', 'brown', 'grey',
-              'c', "gold", "fuchsia", "lime", "darkred", "tomato", "navy"]
-    
-    # If no legend provided, create default legend
-    unique_labels = np.unique(labels)
-    if legend is None:
-        legend = [f"Class {l}" for l in unique_labels]
-    
-    if ax is None:
-        _, ax = plt.subplots(1,1,figsize=(12,12))
-    
-    # Create a mapping from label values to indices
-    label_to_idx = {label: idx for idx, label in enumerate(unique_labels)}
-    
-    for i, l in enumerate(unique_labels):
-        idx = np.where(labels == l)
-        # Use the label's position in unique_labels to index into legend
-        ax.scatter(points[idx, 0], points[idx, 1], label=legend[i], c=COLORS[i % len(COLORS)])
-    ax.legend(loc="center left")
 
 
 class NormLayer(nn.Module):
@@ -239,38 +219,62 @@ class Trainer:
                 break
         return
 
+def display_projections(points, labels, ax=None, legend=None):
+    """ Displaying low-dimensional data projections """
+    
+    COLORS = ['r', 'b', 'g', 'y', 'purple', 'orange', 'k', 'brown', 'grey',
+              'c', "gold", "fuchsia", "lime", "darkred", "tomato", "navy"]
+    
+    # If no legend provided, create default legend
+    unique_labels = np.unique(labels)
+    if legend is None:
+        legend = [f"Class {l}" for l in unique_labels]
+    
+    if ax is None:
+        _, ax = plt.subplots(1,1,figsize=(12,12))
+    
+    for i, l in enumerate(unique_labels):
+        idx = np.where(labels == l)
+        # Use the label's position in unique_labels to index into legend
+        ax.scatter(points[idx, 0], points[idx, 1], label=legend[i], c=COLORS[i % len(COLORS)])
+    ax.legend(loc="best")
 
-def plot_PCA(imgs_flat, embs, labels, legend):
+def plot_both(imgs_flat, embs, labels, legend):
     """ Plotting PCA of images and embeddings """
     pca_imgs = PCA(n_components=2).fit_transform(imgs_flat)
     pca_embs = PCA(n_components=2).fit_transform(embs)
-
-    plt.style.use('seaborn')
-    fig, ax = plt.subplots(1,2,figsize=(26,10))
-
-    
-    N = len(labels)  # Use all points
-    display_projections(pca_imgs[:N], labels[:N], ax=ax[0], legend=legend)
-    ax[0].set_title("PCA Proj. of Images")
-    display_projections(pca_embs[:N], labels[:N], ax=ax[1], legend=legend)
-    ax[1].set_title("PCA Proj. of Embeddings")
-    plt.show()
-    return
-
-
-def plot_TSNE(imgs_flat, embs, labels, legend):
     N = 2000
     tsne_imgs = TSNE(n_components=2).fit_transform(imgs_flat[:N])
     tsne_embs = TSNE(n_components=2).fit_transform(embs[:N])
+ 
+    fig, ax = plt.subplots(2,2)
+    fig.set_size_inches(32,16)
 
-    fig,ax = plt.subplots(1,2,figsize=(26,10))
-    display_projections(tsne_imgs[:N], labels[:N], ax=ax[0], legend=legend)
-    ax[0].set_title("T-SNE Proj. of Images")
-    display_projections(tsne_embs[:N], labels[:N], ax=ax[1], legend=legend)
-    ax[1].set_title("T-SNE Proj. of Embeddings")
+    N = len(labels)  # Use all points
+    # print(f"N: {N}")
+    # print(f"len(legend): {len(legend)}")
+    display_projections(pca_imgs[:N], labels[:N], ax=ax[0,0], legend=legend)
+    ax[0,0].set_title("PCA Proj. of Images")
+    display_projections(pca_embs[:N], labels[:N], ax=ax[0,1], legend=legend)
+    ax[0,1].set_title("PCA Proj. of Embeddings")
+    display_projections(tsne_imgs[:N], labels[:N], ax=ax[1,0], legend=legend)
+    ax[1,0].set_title("T-SNE Proj. of Images")
+    display_projections(tsne_embs[:N], labels[:N], ax=ax[1,1], legend=legend)
+    ax[1,1].set_title("T-SNE Proj. of Embeddings")
+    plt.tight_layout()
     plt.show()
-    return tsne_imgs, tsne_embs
+    return tsne_embs
 
+def plot_all(imgs_flat, embs, labels, labels_mask, legend, stats):
+
+    visualize_progress(stats["train_loss"], stats["valid_loss"], start=0)
+    tsne_embs = plot_both(imgs_flat[labels_mask], embs[labels_mask], labels[labels_mask], legend)
+    # tsne_embs = plot_TSNE(imgs_flat[labels_mask], embs[labels_mask], labels[labels_mask], legend)
+
+    # Get the indices of the filtered data for proper dataset indexing
+    filtered_indices = np.where(labels_mask)[0]
+    
+    return tsne_embs, filtered_indices
 
 def calculate_ARI(imgs_flat, embs, labels):
     kmeans_imgs = KMeans(n_clusters=10, n_init=10,random_state=0).fit(imgs_flat)
@@ -280,29 +284,28 @@ def calculate_ARI(imgs_flat, embs, labels):
     print(f"Clustering images achieves  ARI={round(ari_imgs*100,2)}%")
     print(f"Clustering embeddings achieves ARI={round(ari_embs*100,2)}%")
 
-def display_projections_images(points, labels, dataset, ax=None, legend=None):
-    """ Displaying low-dimensional data projections using images instead of points """
 
+def display_projections_images(points, labels, dataset, ax=None, legend=None, indices=None):
+    """ Displaying low-dimensional data projections using images instead of points """
+    from matplotlib.offsetbox import OffsetImage, AnnotationBbox
     COLORS = ['r', 'b', 'g', 'y', 'purple', 'orange', 'k', 'brown', 'grey',
-              'c', "gold", "fuchsia", "lime", "darkred", "tomato", "navy",
-              "pink", "cyan", "magenta", "olive", "teal", "coral", "indigo", 
-              "violet", "maroon", "turquoise", "salmon", "plum", "sienna", 
-              "orchid", "peru", "mediumseagreen", "lightcoral", "darkgreen", 
-              "darkblue", "darkviolet", "darkorange", "darkcyan", "darkmagenta"]
-    
+              'c', "gold", "fuchsia", "lime", "darkred", "tomato", "navy"]
+
     legend = [f"Class {l}" for l in np.unique(labels)] if legend is None else legend
     _, ax = plt.subplots(1,1,figsize=(36,24))
     
-    # Plot points
     for i, l in enumerate(np.unique(labels)):
-        idx = np.where(labels == l)
-        ax.scatter(points[idx, 0], points[idx, 1], label=legend[i], color=COLORS[i % len(COLORS)])
-    
-    # Add image thumbnails
+        idx = np.where(l==labels)
+        ax.scatter(points[idx, 0], points[idx, 1], label=legend[i], color=COLORS[i]) 
+
     for i, point in enumerate(points):
         xy = [point[0], point[1]]
         
-        arr_img = dataset[i][0][0].permute(1, 2, 0).numpy()
+            # Use indices mapping if provided, otherwise use direct indexing
+        dataset_idx = indices[i] if indices is not None else i
+        arr_img = dataset[dataset_idx][0][0].permute(1, 2, 0).numpy()
+
+        # arr_img = dataset[i][0][0][0]
         l = labels[i]
         imagebox = OffsetImage(arr_img, zoom=1)
         imagebox.image.axes = ax
@@ -311,12 +314,14 @@ def display_projections_images(points, labels, dataset, ax=None, legend=None):
                             xycoords='data',
                             boxcoords="offset points",
                             pad=0.1,
-                            bboxprops=dict(edgecolor=COLORS[l], lw=2)
+                            bboxprops=dict(edgecolor=COLORS[l % len(COLORS)], lw=2)
+
                             )
 
         ax.add_artist(ab)
     ax.legend(loc="best")
     plt.show()
+
 
 class TripletDataset:
     """
@@ -362,20 +367,107 @@ class TripletDataset:
             
         return (anchor_img, pos_img, neg_img), (anchor_lbl, pos_lbl, neg_lbl)
     
-def get_embeddings(model, test_loader, device):
+def get_embeddings(model, test_loader, device, simclr=False):
     imgs_flat = []
     embs = []
     labels = []
-    with torch.no_grad():
-        for (anchor, _, _), (lbl,_, _) in test_loader:
-            anchor = anchor.to(device)
-            anchor_emb = model.forward_one(anchor)
-            
-            labels.append(lbl)
-            embs.append(anchor_emb.cpu())
-            imgs_flat.append(anchor.cpu().flatten(1))
+    model = model.eval()
 
-    labels = np.concatenate(labels)
-    embs = np.concatenate(embs)
-    imgs_flat = np.concatenate(imgs_flat)
+    with torch.no_grad():
+        if not simclr:
+            for (anchor, _, _), (lbl,_, _) in test_loader:
+                anchor = anchor.to(device)
+                anchor_emb = model.forward_one(anchor)
+                
+                labels.append(lbl)
+                embs.append(anchor_emb.cpu())
+                imgs_flat.append(anchor.cpu().flatten(1))
+
+        else: 
+            for img, lbl in tqdm(test_loader):
+                imgs = img.to(device)
+                emb = model.backbone(imgs)
+                
+                labels.append(lbl)
+                embs.append(emb.cpu())
+                imgs_flat.append(img.cpu().flatten(1))
+
+        labels = np.concatenate(labels)
+        embs = np.concatenate(embs)
+        imgs_flat = np.concatenate(imgs_flat)
+
     return imgs_flat, embs, labels
+
+
+############ SimCLR ############
+
+class ContrastiveTransform:
+    """
+    Data augmentation for contrastive learning
+    """
+    def __init__(self, test=False):
+        if test:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Resize((64, 64)),
+            ])
+        else:
+            self.transform = transforms.Compose([
+                transforms.ToTensor(),
+                transforms.RandomResizedCrop(64, scale=(0.5, 1.33), antialias=True),
+                transforms.RandomRotation(20),
+                transforms.RandomHorizontalFlip(p=0.5),
+                transforms.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.4, hue=0.1)
+                                ])
+
+    def __call__(self, x):
+        """ Given an Image, returning two augmented versions of the same image """
+        return self.transform(x), self.transform(x)
+
+class SimCLRDataset():
+    """
+    Wrapper to return pairs of augmented images
+    """
+    
+    def __init__(self, dataset , test=False):
+        self.dataset = dataset
+        self.transform = ContrastiveTransform(test=test)
+
+        # Calculate dimensions for reshaping
+        total_pixels = dataset.shape[1] // 3
+        self.height = int(total_pixels ** 0.5)  # Assuming square images
+        self.width = total_pixels // self.height
+        return
+    
+    def __len__(self):
+        return len(self.dataset)
+    
+    def __getitem__(self, idx):
+
+        anchor_img = self.dataset[idx].reshape(self.height, self.width, 3)  # Reshape to HxWx3
+        
+        view1, view2 = self.transform(anchor_img)  # this will call the __call__ method of the ContrastiveTransform class
+        return view1, view2
+
+
+def nt_xent_loss(z_i, z_j, temperature=0.1):
+    """
+    Normalized Temperature-scaled Cross Entropy Loss
+    """
+    B, dim = z_i.shape
+    
+    # Concatenate positive pairs
+    z = torch.cat([z_i, z_j], dim=0)  # Shape: (2 * B, dim)
+    # Compute similarity matrix
+    sim_matrix = torch.mm(z, z.T) / temperature  # Shape: (2 * B, 2 * B)
+    
+    # Mask to remove self-similarity (diagonal)
+    mask = torch.eye(2 * B, dtype=torch.bool).to(z.device)
+    sim_matrix_final = sim_matrix.masked_fill(mask, float('-inf'))
+    sim_matrix_vis = sim_matrix
+
+    positives_labels = torch.cat([torch.arange(B, 2*B), torch.arange(0, B)]).to(z.device)
+
+    # Compute cross entropy loss
+    loss = F.cross_entropy(sim_matrix_final, positives_labels, reduction="mean")
+    return loss, sim_matrix_vis
