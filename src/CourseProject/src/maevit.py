5d074c9b74f5cd48c8a0ca4e5e0b900f66ef271f
AmirhosseinSoltan <ah.soltani2010@gmail.com>
2025-09-14 15:39:31 +0000

    paceholders for futrue direction added

--- src/CourseProject/src/maevit.py
+++ src/CourseProject/src/maevit.py
@@ -4,5 +4,14 @@
 class MaskedAutoencoderViT(nn.Module):
     """ Masked Autoencoder with VisionTransformer backbone
     """
-    def __init__():
+    def __init__(self):
+        
+    
+        # self.encoder = None
+        # self.decoder = None
+        # self.predictor = Predictor(
+        #                             embed_dim=embed_dim,
+        #                             num_heads=4,
+        #                             mlp_size=256
+        #                             ) if use_predictor else nn.Identity()
         pass
