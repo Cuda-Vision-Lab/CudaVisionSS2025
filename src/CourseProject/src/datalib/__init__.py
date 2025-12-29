215b0f1215921243c9fff43bc371fc2dd5f349ee
Amirhossein <ah.soltani2010@gmail.com>
2025-12-29 15:10:56 +0100

    cleared everything to merge the project repo

--- src/CourseProject/src/datalib/__init__.py
+++
@@ -1,8 +0,0 @@
-'''
-With __init__.py, Python treats the folder as a package so you can import from it (e.g., from data import load_data). 
-Enables relative imports inside the package:
-'''
-
-from .load_data import load_data, build_data_loader
-from .MoviC import MOVIC
-
